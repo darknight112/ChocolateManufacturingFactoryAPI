c1e6d811cf97261060773e47c3d006327bc380c4
darknight112 <hkalamri@gmail.com>
2023-05-21 11:50:07 +0400

    now we can create new products by using create product api api/products

--- AutoCommitScript.bat
+++ AutoCommitScript.bat
@@ -6,7 +6,7 @@ cd C:\Users\Lenovo\Desktop\Task-3  # Replace with the path to your repository
 git add .
 
 # Commit the changes
-git commit -m "Create service for order and ProductController class, also add save product feature" --allow-empty
+git commit -m "now we can create new products by using create product api api/products" --allow-empty
 
 # Push the changes to GitHub
 git push
--- src/main/java/com/Hisham/Task3/Controller/ProductController.java
+++ src/main/java/com/Hisham/Task3/Controller/ProductController.java
@@ -4,14 +4,28 @@ import com.Hisham.Task3.Models.Product;
 import com.Hisham.Task3.RequestObject.GetProductRequestObject;
 import com.Hisham.Task3.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     ProductService productService;
+
+    @PostMapping("api/products")
+    public void saveProduct (@RequestBody GetProductRequestObject productRequestObject) {
+        createProduct(productRequestObject);
+    }
+
+//    @GetMapping("api/products")
+//    public List<Product> getProduct () {
+//        return productService.getProduct();
+//    }
 
     public void createProduct(GetProductRequestObject productRequestObject) {
 
