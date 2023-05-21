159046eed4ccc152093ba7cbe6174235834d8e25
darknight112 <hkalamri@gmail.com>
2023-05-21 10:19:33 +0400

    Initializing project

---
+++ AutoCommitScript.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd C:\Users\Lenovo\Desktop\Task-3  # Replace with the path to your repository
+
+# Add your files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Auto update" --allow-empty
+
+# Push the changes to GitHub
+git push
