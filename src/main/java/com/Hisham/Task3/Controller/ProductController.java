efca390590b1efd7702413b7baac773958ee1645
darknight112 <hkalamri@gmail.com>
2023-05-21 16:07:16 +0400

    add delete by id feature for product class by using api/products/delete/(productID)

--- src/main/java/com/Hisham/Task3/Controller/ProductController.java
+++ src/main/java/com/Hisham/Task3/Controller/ProductController.java
@@ -5,6 +5,7 @@ import com.Hisham.Task3.RequestObject.GetProductRequestObject;
 import com.Hisham.Task3.ResponseObject.GetProductResponse;
 import com.Hisham.Task3.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -30,10 +31,21 @@ public class ProductController {
         return productService.getProductById(productId);
     }
 
+    @PutMapping("/api/products/{productId}")
+    public ResponseEntity<GetProductResponse> updateProduct(@PathVariable Long productId, @RequestBody GetProductRequestObject updatedProduct) {
+        GetProductResponse response = productService.updateProduct(productId, updatedProduct);
+        if (response != null) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @RequestMapping("api/products/delete/{productId}")
     public void deleteProduct (@PathVariable Long productId) {
         productService.deleteProductById(productId);
     }
+
 
     public void createProduct(GetProductRequestObject productRequestObject) {
 
--- src/main/java/com/Hisham/Task3/Service/ProductService.java
+++ src/main/java/com/Hisham/Task3/Service/ProductService.java
@@ -2,6 +2,7 @@ package com.Hisham.Task3.Service;
 
 import com.Hisham.Task3.Models.Product;
 import com.Hisham.Task3.Reposetory.ProductRepository;
+import com.Hisham.Task3.RequestObject.GetProductRequestObject;
 import com.Hisham.Task3.ResponseObject.GetProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,21 @@ public class ProductService {
         return productRepository.findAll();
     }
 
+    public GetProductResponse updateProduct(Long productId, GetProductRequestObject updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(updatedProduct.getName());
+            product.setPrice(updatedProduct.getPrice());
+            product.setIngredients("NOT IMPORTANT");
+            product.setQuantity(updatedProduct.getQuantity());
+            productRepository.save(product);
+            return getProductById(productId);
+        }
+        return null;
+    }
+
+
     public GetProductResponse getProductById(Long productId) {
         Optional<Product> optionalProduct =  productRepository.findById(productId);
         if(!optionalProduct.isEmpty())
@@ -33,10 +49,7 @@ public class ProductService {
             productResponse.setQuantity(product.getQuantity());
             return productResponse;
         }
-
-
         return null;
-
     }
 
     public void deleteProductById(Long productId) {
