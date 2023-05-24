3e360faa7a91616f5260dd925cd62501ce71aa11
darknight112 <hkalamri@gmail.com>
2023-05-24 10:54:49 +0400

    create place order method that can take the id of the product and retrive the info of the data and make the changes in database

--- AutoCommitScript.bat
+++ AutoCommitScript.bat
@@ -6,7 +6,7 @@ cd C:\Users\Lenovo\Desktop\Task-3  # Replace with the path to your repository
 git add .
 
 # Commit the changes
-git commit -m "add description" --allow-empty
+git commit -m "create place order method that can take the id of the product and retrive the info of the data and make the changes in database" --allow-empty
 
 # Push the changes to GitHub
 git push
